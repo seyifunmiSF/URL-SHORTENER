3c04ba1f3da67d0deaeb78ee54c5ce4c977b2bb1
Seyifunmi Ajayi <seyifunmiajayi.sa@gmail.com>
2023-07-09 00:26:37 +0100

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIwOTY1NA/spectral.js?branch_slug=main&token=2b6e3bbf-5152-421c-a187-3137289f6cc4";
+export default { extends: ruleset };
